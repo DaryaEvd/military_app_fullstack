b75f10d1a7e2edcf1385f299a4731ccb89991724
DaryaEvd <dhfingerstyle@gmail.com>
2024-05-17 13:48:02 +0700

    removed cached repo

---
+++ frontend/military_front/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <h1>This is military database project</h1>
+        <button onClick={() => alert('Start button clicked!')}>Start</button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
