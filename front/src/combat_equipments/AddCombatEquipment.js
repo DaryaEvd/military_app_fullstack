de2defa87c8845a7e5f958c0dd8b8c51bd79fbbf
DaryaEvd <dhfingerstyle@gmail.com>
2024-05-20 12:06:33 +0700

    added data to comb eq form

--- front/src/combat_equipments/AddCombatEquipment.js
+++ front/src/combat_equipments/AddCombatEquipment.js
@@ -1,9 +1,24 @@
 import React from 'react'
 
 export default function AddCombatEquipment() {
-  return (
-    <div>
-      AddCombatEquipment Form
-    </div>
-  )
+    return (
+        <div className='container'>
+            <div className='row'>
+                <div className="col-md-6 offset-md-3 border rounded p-4 mt-2 shadow">
+                    <h2 className='text-center m-4'>Register Combat Equipment</h2>
+                    <div className='mb-3'>
+                        <label htmlFor='Name' className='form-label'>
+                            Name
+                        </label>
+                        <input
+                            type={"text"}
+                            className='form-control'
+                            placeholder='Enter name of combat equipment'
+                            name="name"
+                        />
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
 }
