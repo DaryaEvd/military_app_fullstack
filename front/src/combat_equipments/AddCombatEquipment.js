186577e39c00da572513960fe7367c0ec8a2e3cc
DaryaEvd <dhfingerstyle@gmail.com>
2024-05-20 12:30:44 +0700

    added more fields for form + 2 btns on combat equpment

--- front/src/combat_equipments/AddCombatEquipment.js
+++ front/src/combat_equipments/AddCombatEquipment.js
@@ -17,6 +17,75 @@ export default function AddCombatEquipment() {
                             name="name"
                         />
                     </div>
+
+                    <div className='mb-3'>
+                        <label htmlFor='Name' className='form-label'>
+                            Name
+                        </label>
+                        <input
+                            type={"text"}
+                            className='form-control'
+                            placeholder='Enter name of combat equipment'
+                            name="name"
+                        />
+                    </div>
+
+                    <div className='mb-3'>
+                        <label htmlFor='Name' className='form-label'>
+                            Experience of using
+                        </label>
+                        <input
+                            type={"text"}
+                            className='form-control'
+                            placeholder='Enter experience of using of combat equipment'
+                            name="name"
+                        />
+                    </div>
+
+                    <div className='mb-3'>
+                        <label htmlFor='Name' className='form-label'>
+                            Condition of vehicle
+                        </label>
+                        <input
+                            type={"text"}
+                            className='form-control'
+                            placeholder='Enter condition of vehicle'
+                            name="name"
+                        />
+                    </div>
+
+                    <div className='mb-3'>
+                        <label htmlFor='Name' className='form-label'>
+                            Number of seats
+                        </label>
+                        <input
+                            type={"text"}
+                            className='form-control'
+                            placeholder='Enter number of seats'
+                            name="name"
+                        />
+                    </div>
+
+                    <div className='mb-3'>
+                        <label htmlFor='Name' className='form-label'>
+                            Name of vehicle
+                        </label>
+                        <input
+                            type={"text"}
+                            className='form-control'
+                            placeholder='Enter name of vehicle'
+                            name="name"
+                        />
+                    </div>
+
+                    <button type="submit" className="btn btn-outline-primary">
+                        Submit
+                    </button>
+
+                    <button type="submit" className="btn btn-outline-danger mx-2">
+                        Cancel
+                    </button>
+
                 </div>
             </div>
         </div>
